8100cb5cb09781f41462960bc9ea494317f7970a
SomeBreadBug <71806357+SomeBreadBug@users.noreply.github.com>
2024-08-26 19:17:15 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,6 +1,6 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
+	name: "The Something Tree",
+	id: "something",
 	author: "nobody",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
